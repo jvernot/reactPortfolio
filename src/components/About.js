0024073b75d99b98ec4908f669fbf1bf1d9c43d6
Jason Vernot <jason.vernot@gmail.com>
2020-07-15 22:25:46 -0400

    uploaded react components

---
+++ src/components/About.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    console.log(resumeData.aboutme);
+    return (
+      <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+          </div>
+
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>{resumeData.aboutme}</p>
+
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{resumeData.name}</span>
+                  <br></br>
+                  <a
+                    href={`mailto: ${resumeData.email}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {resumeData.email}
+                  </a>
+                  <br></br>
+                  <a
+                    href={resumeData.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Github Profile
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,105 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/jvernot",
+    "name": "Jason Vernot",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am an eager full-stack developer completing the UNC-Chapel Hill Coding Bootcamp.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jason-vernot/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jvernot",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am working towards the completion of the University of North Carolina Coding Bootcamp where I have gained skills in HTML5, CSS3, Javascript, JQuery, Bootstrap, AJax, JSON, Node.js, Express.js, Mysql, Mongo DB and React.js. My background is in digtal marketing, which has given me invaluable experience in a professional, collaboartive, and client-facing environment. My direct expereicne with ecommerce clients has given me exposure to professional development work, and helped fuel the path that I'm on now. I am really excited about the coding skills and knowledge that I've gained as well as the work I've compelted already. But now, I'm looking to take the next step and begin my career as a developer. So feel free to download my resume, check out my work, or send me a message. Whether you want to connect about an opportunity, or just chat about projects and past expereinces, I'm eager to talk.",
+    "email":"jason.vernot@gmail.com",
+    "website":"https://github.com/jvernot",
+    "education":[
+      {
+        "UniversityName":"North Carolina State University",
+        "specialization":"Business Administration - Marketing",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company2",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"GameGo",
+        "description":"GameGo is a full-stack web app to help users find and save new video games they want to play. This isn't like any old video game search, however. With GameGo, you input your game criteria and we'll server you up random games that match. Find a game you like and saved it to your own list to come back and view at any time. The app is built using express.js, jQuery, mySQL, sequelize, handlebars.js, Bootstrap, and the RAWG API.",
+        "imgurl":"./images/GameGo2.png",
+        "appurl":"https://game-go-randomizer.herokuapp.com/"
+      },
+      {
+        "name":"Eat-Da-Burger",
+        "description":"Eat-Da-Burger is a restaurant app that lets users input the names of burgers they'd like to eat. The devour button also allows users to keep track of burgers they've already tried! The app uses express.js, mySQL, a custom ORM, jQuery, handlebars.js, and Materialze.",
+        "imgurl":"./images/Eat-Da-Burger.png",
+        "appurl":"https://eat-da-burger21.herokuapp.com/"
+      },
+      {
+        "name":"What's For Dinner?",
+        "description":"Can't figure out whether you want to cook with the things you have at home or go out to eat? The What's For Dinner app can help you choose. Search for recipes by ingredient based on what you have at home. If you don't find anything you like search for restaurants by cuisine wherever you are! The app is built using HTML5, CSS3, Materialize, JavaScript, jQuery, and two third party APIs: Spoonacular and Zomato.",  
+        "imgurl":"./images/DinnerApp.png",
+        "appurl":"https://jvernot.github.io/wineAndDine/"
+      },
+      {
+        "name":"Note Taker",
+        "description":"The Note Taker App allows users to create and save notes all in one convenient place. This simple app is built on express.js and follows the CRUD method, allowing users to create, update, and delete their notes.",
+        "imgurl":"./images/portfolio/NoteTaker.png",
+        "appurl":"https://notetaker-app21.herokuapp.com/"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy1"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
